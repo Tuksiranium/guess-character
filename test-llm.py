e388edf4e1b27a2ec5ca98453748fe99d9e321cd
Alexandre Agostinho <alexandre.agostinho@etu.uca.fr>
2025-01-29 09:54:06 +0100

    add some code

--- test-llm.py
+++ test-llm.py
@@ -1,13 +1,24 @@
-import transformers
+from transformers import Text2TextGenerationPipeline, GenerationConfig, AutoModelForCausalLM, AutoTokenizer
 import torch
 
 model_id = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
-
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
-    device_map="auto"
+model = AutoModelForCausalLM.from_pretrained(model_id)
+generation_config = GenerationConfig(
+    max_new_tokens=200,
+    do_sample=True,
+    temperature=.6,
+    top_p=.92,
+    top_k=0
 )
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-pipeline("Hey, how are you doing today?")
+inputs = tokenizer("Tell me the temperature a clermont-ferrand aujourd'hui", return_tensors="pt")
+outputs = model.generate(**inputs, generation_config=generation_config)
+
+print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+
+# inp = input("> ")
+# while inp != '\n':
+#     gen = model.generate
+#     print("Bot: " + gen[0]['generated_text'])
+#     inp = input("> ")
